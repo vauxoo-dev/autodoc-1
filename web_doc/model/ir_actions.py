b4d8c33d391024085a2e32adee71ba14e8e7b726
Nhomar Hernandez <nhomar@gmail.com>
2013-04-30 13:13:57 -0700

    [IMP] Just declarated linktome, TODO: Create automagically the link

--- web_doc/model/ir_actions.py
+++ web_doc/model/ir_actions.py
@@ -6,6 +6,15 @@ class act_window(osv.osv):
     '''Adding link to doc'''
     
     _inherit = 'ir.actions.act_window'
+
+    def _get_linktome(self, cr, uid, ids, name, args, context=None):
+        if context is None:
+            context = {}
+        res = {}
+        for i in ids:
+            res[i] = "hola.txt"
+        return res
+
     _columns = {
     'doc': fields.many2one('document.page', 'Long Doc.',
         domain = [('type', '=', 'content')],
@@ -13,6 +22,8 @@ class act_window(osv.osv):
     'doc_body': fields.related('doc', 'content', type='text',
         string = 'Doc', relation = 'dojcument.page', store = True,
         help = '''Documentation related to the document page element'''),
+    'linktome': fields.function(_get_linktome, 'Link to me',
+        help = """Link to edit me"""),
     }
 
 
