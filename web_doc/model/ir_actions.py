77e916c889dd74bb4d054de81e08df5374afadaa
Nhomar Hernandez <nhomar@gmail.com>
2013-04-29 22:36:13 -0700

    [ADD] New Field to action.

---
+++ web_doc/model/ir_actions.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+from openerp.osv import osv, fields
+
+class act_window(osv.osv):
+    
+    '''Adding link to doc'''
+    
+    _inherit = 'ir.actions.act_window'
+    _columns = {
+    'doc':fields.many2one('document.page', 'Long Doc.', help='''
+                                                        Link to document 
+                                                        page of this action.
+                                                        '''), 
+    }
+
